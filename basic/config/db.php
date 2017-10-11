6ba39dd8c8d961333afa360ba4ac34f3d8c5dc77
wzh190015 <734649192@qq.com>
2017-10-11 22:38:12 +0800

    modify gitignore

--- basic/config/db.php
+++ basic/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '',
+    'password' => 'p0P)o9O(i8I*',
     'charset' => 'utf8',
 ];
